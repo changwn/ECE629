d5dc56bf718882dd31836f3a597068383cbdfb4e
changwn <changwn.nku@gmail.com>
2020-10-24 17:34:44 -0400

    Add files via upload

--- proj1a.py
+++ proj1a.py
@@ -6,7 +6,7 @@ Created on Wed Oct 14 22:39:54 2020
 """
 
 import os
-#os.chdir('C:/Users/wnchang/Documents/F/PhD_Course/ECE629_NN/4.Project/Proj1')
+os.chdir('C:/Users/wnchang/Documents/F/PhD_Course/ECE629_NN/4.Project/Proj1')
 import sys
 import numpy as np
 import time
@@ -36,7 +36,7 @@ def main():
     
     
     n_feature = 1  # the number of input neurons
-    n_hidden = 5000  # the number of hidden neurons
+    n_hidden = 500  # the number of hidden neurons
     n_output = 1  # the number of output neurons
     net = Net(n_feature, n_hidden, n_output)  # define the network
     print(net)  # net architecture
@@ -44,90 +44,65 @@ def main():
     
     x= torch.unsqueeze(torch.linspace(-24, 24, 1000),dim=1)  # x data (tensor), shape=(100, 1)
     y = torch.sin(PI * x/12)  + 100   
-    #T=1
-    #y = 100 + torch.cos(2*PI*(x/T)) # function in task2
+
     
     x, y = Variable(x), Variable(y)
-#    plt.scatter(x.data.numpy(), y.data.numpy())
+
     
-#    #another way to define a network
-#    net = torch.nn.Sequential(
-#            torch.nn.Linear(1, 5000),
-#            torch.nn.ReLU(),
-##            torch.nn.Linear(200, 100),
-##            torch.nn.ReLU(),
-#            torch.nn.Linear(5000, 1),
-#        )
+
     
-    optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
+    optimizer = torch.optim.Adam(net.parameters(), lr=0.05)
     loss_func = torch.nn.MSELoss()  # this is for regression mean squared loss
     
-    BATCH_SIZE = 1000
-    EPOCH = 3000
-    PLOT_EPOCH = 500
+#    BATCH_SIZE = 100
+    EPOCH = 6000
+    PLOT_EPOCH = 1500
     
-    torch_dataset = Data.TensorDataset(x, y)
-    
-    loader = Data.DataLoader(
-        dataset=torch_dataset, 
-        batch_size=BATCH_SIZE, 
-        shuffle=True, num_workers=2,)
-    
-    #my_images = []
-    #fig, ax = plt.subplots(figsize=(16,10))
-    
-    plt.figure(figsize=(10,10))
-#    plt.figure(figsize=(10,10), dpi=80)
-#    plt.figure(1)
-#    figs = list()
-    i=1    
+
     lossValue = []
     # start training
     start = time.time()
     for epoch in range(1, EPOCH+1):
         
-        if epoch % 50 == 0:
+        if epoch % 100 == 0:
                 print("epoch: ", epoch )
         
-        for step, (batch_x, batch_y) in enumerate(loader): # for each training step
+#        for step, (batch_x, batch_y) in enumerate(loader): # for each training step
             
-            b_x = Variable(batch_x)
-            b_y = Variable(batch_y)
-    
-            prediction = net(b_x)     # input x and predict based on x
-    
-            loss = loss_func(prediction, b_y)     # must be (1. nn output, 2. target)
-            lossValue.append(loss)
-    
-            optimizer.zero_grad()   # clear gradients for next train
-            loss.backward()         # backpropagation, compute gradients
-            optimizer.step()        # apply gradients
+#        b_x = Variable(batch_x)
+#        b_y = Variable(batch_y)
+        b_x = x
+        b_y = y
+
+        prediction = net(b_x)     # input x and predict based on x
+
+        loss = loss_func(prediction, b_y)     # must be (1. nn output, 2. target)
+        lossValue.append(loss)
+
+        optimizer.zero_grad()   # clear gradients for next train
+        loss.backward()         # backpropagation, compute gradients
+        optimizer.step()        # apply gradients
+        
+        if epoch % PLOT_EPOCH == 0:
+
             
-            if epoch % PLOT_EPOCH == 0:
-                # plot and show learning process
-#                ax = "ax"+str(i)
-#                ax = plt.subplot(3, 2, i)
-                plt.subplot(3, 2, i)
-                i+=1
-                
-#                z = np.sqrt(x**2)
-#                plt.ylim(95, 105)
-                plt.scatter(x.data.numpy(), y.data.numpy(), c='r', alpha=0.5, s=0.5)
-#                plt.scatter(x.data.numpy(), y.data.numpy())
-                plt.scatter(b_x.detach().numpy(), prediction.detach().numpy(), c='b',alpha=0.5, s=0.5)
-                plt.legend(["y","y_hat"])
-#                plt.plot(b_x.detach().numpy(), b_y.detach().numpy(), 'c')
-#                plt.text(12, 95.3, 'Loss=%.4f' % loss.data.numpy(),fontdict={'size': 10,'color': 'red'})
-                title = "Epoch:"+str(epoch)+" Hidden Neurons:"+str(1000)
-                plt.title(title, fontsize="small", fontweight="bold")   
+            plt.figure(figsize=(6,6))
+            plt.scatter(x.data.numpy(), y.data.numpy(), c='r', alpha=0.5, s=0.5)
+            plt.scatter(b_x.detach().numpy(), prediction.detach().numpy(), c='b',alpha=0.5, s=0.5)
+            title = "Epoch:"+str(epoch)+" Hidden Neurons:"+str(50)
+            plt.title(title, fontsize="small", fontweight="bold")  
+            plt.legend(["y","y_hat"])
+            timestr = time.strftime("%Y%m%d-%H%M%S")
+            imgName = "./Task1_" + timestr + ".png"
+            plt.savefig(imgName)
             
     end = time.time()
     print("Training time: ", end - start) 
             
 
-    timestr = time.strftime("%Y%m%d-%H%M%S")
-    imgName = "./Task1_" + timestr + ".png"
-    plt.savefig(imgName)
+#    timestr = time.strftime("%Y%m%d-%H%M%S")
+#    imgName = "./Task1_" + timestr + ".png"
+#    plt.savefig(imgName)
     
     plt.figure(figsize=(7,7))
     imgLoss = "./Task_1_loss" + timestr + ".png"
